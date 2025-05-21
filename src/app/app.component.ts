109f8f5774ddc4da134f4ad6447dedd005721985
Caua emanuel <emanuellcaua8@gmail.com>
2025-05-21 09:52:40 -0300

    inicio do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'list';
+   todoList: TodoItem[]=[];
+   newTask: string = '';
+
+   addTask():void{
+    if(this.newTask.trim() !== ''){
+      const newTodo: TodoItem = {
+        id: Date.now(),
+        title: this.newTask,
+        completed: false
+      };
+      console.log(newTodo);
+      this.todoList.push(newTodo);
+      this.newTask = '';
+    }
+   }
 }
